f11ecd854fbc37540899d918d0e4172b175a9382
rootzoll <christian@geektank.de>
2015-10-29 00:23:34 +0100

    added ngcordova and geolocation plugin
    OK there is something not optimal with bower imports - so I copied
    ngCordova lib by hand over www … needs to be fixed later on - now just
    going forward

--- www/js/app.js
+++ www/js/app.js
@@ -5,21 +5,36 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ngCordova'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $cordovaGeolocation, $log) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
-
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleLightContent();
     }
+
+    /*
+     * TEST
+     */
+    var posOptions = {timeout: 10000, enableHighAccuracy: false};
+    $cordovaGeolocation
+          .getCurrentPosition(posOptions)
+          .then(function (position) {
+              var lat  = position.coords.latitude
+              var long = position.coords.longitude
+              $log.info("lat("+lat+") long("+long+")");
+          }, function(err) {
+              // error
+            $log.info("GPS ERROR");
+          });
+
   });
 })
 
