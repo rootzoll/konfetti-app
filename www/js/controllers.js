8157c9fa92bfc410e400e16dd630162dd989af98
rootzoll <christian@geektank.de>
2015-10-29 01:47:45 +0100

    lang switching select box

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,33 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope) {
+
+        $scope.actualKonfettiCount = 1000;
+
+        // available app languages
+        $scope.langSet = [
+            {code:'en', display:'EN'},
+            {code:'de', display:'DE'}
+        ];
+
+        // getting app lang from settings (later)
+        $scope.settingsLang = "en";
+
+        // setting selected lang in view to setting
+        $scope.actualLangSelect = $scope.langSet[0];
+        for (i = 0; i < langSet.length; i++) {
+            if (langSet[i].code===$scope.settingsLang) {
+                $scope.actualLangSelect = langSet[i];
+                break;
+            }
+        }
+
+        // receiving changes lang settings --> with i18n
+        $scope.selectedLang = function(selected) {
+            console.log("selected lang: "+selected);
+        };
+
+    })
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
