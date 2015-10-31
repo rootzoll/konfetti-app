2443a83cfc362143ba4a8a7378c5956b17fcaf6f
rootzoll <christian@geektank.de>
2015-10-31 16:42:58 +0100

    3 Notificatios

--- www/js/directives.js
+++ www/js/directives.js
@@ -3,16 +3,8 @@
 angular.module('starter')
     .directive('notification', function () {
         return {
-            scope: {
-                type: '@',
-                moreCallback: '@',
-                deleteCallback: '@'
-            },
             templateUrl: 'templates/directive-notification.html',
             replace: true,
-            restrict: 'A',
-            link: function postLink(scope, element, attrs) {
-                //element.text('this is the menurocket directive');
-            }
+            restrict: 'A'
         };
     });
