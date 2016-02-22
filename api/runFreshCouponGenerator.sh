0ca94a7e923b747ae14686667ffefb4282567137
rootzoll <christian@geektank.de>
2016-02-22 20:55:15 +0100

    Coupon Generator Server Script

---
+++ api/runFreshCouponGenerator.sh
@@ -0,0 +1,8 @@
+killall node
+rm -Rf couponGenerator
+git clone https://github.com/rootzoll/chisel.git couponGenerator
+cd couponGenerator
+npm install
+node src/index.js &
+cd ..
+echo "COUPON GENERATOR STARTED ON PORT 3000"
