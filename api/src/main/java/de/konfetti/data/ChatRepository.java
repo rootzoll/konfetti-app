a139f8ace398637d12c51c6f1dbfcc9aeb56abd5
tnitze <123456>
2016-06-13 16:58:33 +0200

    removed unnecessary annotations

--- api/src/main/java/de/konfetti/data/ChatRepository.java
+++ api/src/main/java/de/konfetti/data/ChatRepository.java
@@ -1,11 +1,9 @@
 package de.konfetti.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface ChatRepository extends JpaRepository<Chat, Long> {
 
     List<Chat> findByRequestId(Long requestId);
--- api/src/main/java/de/konfetti/data/UserRepository.java
+++ api/src/main/java/de/konfetti/data/UserRepository.java
@@ -1,12 +1,10 @@
 package de.konfetti.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 /*
  * Adapter to persist client objects on JPA   
  */
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 //    User findByClientId(Long clientId);
