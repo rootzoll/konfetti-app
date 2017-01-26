d04707827998ec2d1f31fa344f6eeba02d3cc5da
rotzoll <christian@geektank.de>
2017-01-26 10:59:18 +0100

    fix loggly

---
+++ www/lib/angular-loggly-logger/README.md
@@ -0,0 +1,186 @@
+[![Build Status](https://travis-ci.org/ajbrown/angular-loggly-logger.svg)](https://travis-ci.org/ajbrown/angular-loggly-logger)
+[![Coverage Status](https://coveralls.io/repos/ajbrown/angular-loggly-logger/badge.svg?branch=master)](https://coveralls.io/r/ajbrown/angular-loggly-logger?branch=master)
+[![Join the chat at https://gitter.im/ajbrown/angular-loggly-logger](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/ajbrown/angular-loggly-logger?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
+
+
+Angular Loggly Logger is a module which will decorate Angular's $log service,
+and provide a `LogglyLogger` service which can be used to manually send messages
+of any kind to the [Loggly](https://www.loggly.com) cloud log management service.
+
+
+### Getting Started
+
+LogglyLogger can be installed with bower:
+
+```
+bower install angular-loggly-logger
+```
+
+Or with npm:
+
+```
+npm install --save angular-loggly-logger
+```
+
+Once configured (by including "logglyLogger" as a module dependency), the `$log`
+service will automatically be decorated, and all messages logged will be handled
+as normal as well as formated and passed to LogglyLogger.sendMessage.
+The plain text messages are sent into the "json.message" field with the decorated
+log while custom JSON objects are sent via "json.messageObj" field as Loggly
+only supports one type per field.
+
+To use both the decorated $log and the LogglyLogger service, you must first
+configure it with an inputToken, which is done via the LogglyLoggerProvider:
+
+```javascript
+angular.module( 'myApp', [require('angular-loggly-logger')] )
+
+  .config(["LogglyLoggerProvider", function( LogglyLoggerProvider ) {
+    LogglyLoggerProvider.inputToken( '<loggly input token here>' );
+  } ]);
+
+  .run(["LogglyLogger", "$log", function( LogglyLogger, $log ) {
+
+    //This will be sent to both the console and Loggly
+    $log.info( "I'm a little teapot." );
+
+    //This will be sent to loggly only
+    LogglyLogger.sendMessage( { message : 'Short and Stout.' } );
+  }])
+
+```
+
+### $log decoration
+
+When sent through the `$log` decorator, messages will be formatted as follows:
+
+```javascript
+
+// Example: $log.warn( 'Danger! Danger!' );
+{
+  level: "WARN",
+  timestamp: "2014-05-01T13:10Z",
+  msg: "Danger! Danger!",
+  url: "https://github.com/ajbrown/angular-loggly-logger/demo/index.html",
+}
+
+// Example: $log.debug( 'User submitted something:', { foo: 'A.J', bar: 'Space' } )
+
+{
+  level: "DEBUG",
+  timestamp: "2014-05-01T13:18Z",
+  msg: ["User submitted something", { foo: 'A.J.', bar: 'Space' }],
+  url: "https://github.com/ajbrown/angular-loggly-logger/demo/index.html",
+}
+```
+
+> However, 'url' and 'timestamp' are not included by default.  You must enable those options in your application config (see below).
+
+
+Note that if you do not call `LogglyLoggerProvider.inputToken()` in a config method, messages will not be sent to loggly.  At the moment, there is no warning -- your message is just ignored.
+
+### Configuration
+
+The following configuration options are available.
+
+```javascript
+
+  LogglyLoggerProvider
+
+    // set the logging level for messages sent to Loggly.  Default is 'DEBUG',
+    // which will send all log messages.
+    .level( 'DEBUG' )
+
+    // set the token of the loggly input to use.  Must be set, or no logs
+    // will be sent.
+    .inputToken( '<your-token>' )
+
+    // set whether or not HTTPS should be used for sending messages.  Default
+    // is true
+    .useHttps( true )
+
+    // should the value of $location.absUrl() be sent as a "url" key in the
+    // message object that's sent to loggly?  Default is false.
+    .includeUrl( false )
+
+    // should the value of $window.navigator.userAgent be sent as a "userAgent" key in the
+    // message object that's sent to loggly?  Default is false.
+    .includeUserAgent( false )
+
+    // should the current timestamp be included? Default is false.
+    .includeTimestamp( false )
+
+    // set comma-seperated tags that should be included with the log events.
+    // Default is "angular"
+    .inputTag("angular,customTag")
+
+    // Send console error stack traces to Loggly.  Default is false.
+    .sendConsoleErrors( false )
+
+    // Toggle logging to console.  When set to false, messages will not be
+    // be passed along to the original $log methods.  This makes it easy to
+    // keep sending messages to Loggly in production without also sending them
+    // to the console.   Default is true.
+    .logToConsole( true )
+
+    // Custom labels for standard log fields. Use this to customize your log
+    // message format or to shorten your logging payload. All available labels
+    // are listed in this example.
+    .labels({
+      col: 'c',
+      level: 'lvl',
+      line: 'l',
+      logger: 'lgr',
+      message: 'msg',
+      stack: 'stk',
+      timestamp: 'ts',
+      url: 'url',
+      userAgent: 'userAgent'
+    })
+
+```
+
+### Sending JSON Fields
+
+You can also default some "extra/default" information to be sent with each log message.  When this is set, `LogglyLogger`
+will include the key/values provided with all messages, plus the data to be sent for each specific logging request.
+
+```javascript
+
+  LogglyLoggerProvider.fields( { appVersion: 1.1.0, browser: 'Chrome' } );
+
+  //...
+
+  $log.warn( 'Danger! Danger!' )
+
+  >> { appVersion: 1.1.0, browser: 'Chrome', level: 'WARN', message: 'Danger! Danger', url: 'http://google.com' }
+```
+
+Extra fields can also be added at runtime using the `LogglyLogger` service:
+
+```javascript
+  app.controller( 'MainCtrl', ["$scope", "$log", "LogglyLogger", function( $scope, $log, LogglyLogger ) {
+
+    logglyLogger.fields( { username: "foobar" } );
+
+    //...
+
+    $log.info( 'All is good!' );
+
+  >> { appVersion: 1.1.0, browser: 'Chrome', username: 'foobar', level: 'WARN', message: 'All is good', url: 'http://google.com' }
+  }])
+
+```
+
+
+Beware that when using `setExtra` with `LogglyLogger.sendMessage( obj )`, any properties in your `obj` that are the same as your `extra` will be overwritten.  
+
+## ChangeLog
+- v0.2.2 - Fixes preflight cross origin issues.
+- v0.2.3 - Fixes npm install issues related to Bower.
+- v0.2.4 - Adds customizable labels, error stacktraces, and user-agent logging.
+
+
+## Contributing
+
+Contributions are awesome, welcomed, and wanted.  Please contribute ideas by [opening a new issue](http://github.com/ajbrown/angular-loggy-logger/issues), or code by creating a new pull request.  Please make sure your pull request targets the "develop" branch.
---
+++ www/lib/angular-loggly-logger/angular-loggly-logger.js
@@ -0,0 +1,375 @@
+/**
+ *  logglyLogger is a module which will send your log messages to a configured
+ *  [Loggly](http://loggly.com) connector.
+ *
+ *  Major credit should go to Thomas Burleson, who's highly informative blog
+ *  post on [Enhancing AngularJs Logging using Decorators](http://bit.ly/1pOI0bb)
+ *  provided the foundation (if not the majority of the brainpower) for this
+ *  module.
+ */
+; (function( angular ) {
+  "use strict";
+
+  angular.module( 'logglyLogger.logger', [] )
+    .provider( 'LogglyLogger', function() {
+      var self = this;
+
+      var logLevels = [ 'DEBUG', 'INFO', 'WARN', 'ERROR' ];
+
+      var https = true;
+      var extra = {};
+      var includeCurrentUrl = false;
+      var includeTimestamp = false;
+      var includeUserAgent = false;
+      var tag = null;
+      var sendConsoleErrors = false;
+      var logToConsole = true;
+      var loggingEnabled = true;
+      var labels = {};
+
+      // The minimum level of messages that should be sent to loggly.
+      var level = 0;
+
+      var token = null;
+      var endpoint = '://logs-01.loggly.com/inputs/';
+
+        var buildUrl = function () {
+          return (https ? 'https' : 'http') + endpoint + token + '/tag/' + (tag ? tag : 'AngularJS' ) + '/';
+        };
+
+      this.setExtra = function (d) {
+        extra = d;
+        return self;
+      };
+
+      this.fields = function ( d ) {
+        if( angular.isDefined( d ) ) {
+          extra = d;
+          return self;
+        }
+
+        return extra;
+      };
+
+      this.labels = function(l) {
+        if (angular.isObject(l)) {
+          labels = l;
+          return self;
+        }
+
+        return labels;
+      };
+
+      this.inputToken = function ( s ) {
+        if (angular.isDefined(s)) {
+          token = s;
+          return self;
+        }
+
+        return token;
+      };
+
+      this.useHttps = function (flag) {
+        if (angular.isDefined(flag)) {
+          https = !!flag;
+          return self;
+        }
+
+        return https;
+      };
+
+      this.includeUrl = function (flag) {
+        if (angular.isDefined(flag)) {
+          includeCurrentUrl = !!flag;
+          return self;
+        }
+
+        return includeCurrentUrl;
+      };
+
+      this.includeTimestamp = function (flag) {
+        if (angular.isDefined(flag)) {
+          includeTimestamp = !!flag;
+          return self;
+        }
+
+        return includeTimestamp;
+      };
+
+      this.includeUserAgent = function (flag) {
+        if (angular.isDefined(flag)) {
+          includeUserAgent = !!flag;
+          return self;
+        }
+
+        return includeUserAgent;
+      };
+
+      this.inputTag = function (usrTag){
+        if (angular.isDefined(usrTag)) {
+          tag = usrTag;
+          return self;
+        }
+
+        return tag;
+      };
+
+      this.sendConsoleErrors = function (flag){
+        if (angular.isDefined(flag)) {
+          sendConsoleErrors = !!flag;
+          return self;
+        }
+
+        return sendConsoleErrors;
+      };
+
+      this.level = function ( name ) {
+
+        if( angular.isDefined( name ) ) {
+          var newLevel = logLevels.indexOf( name.toUpperCase() );
+
+          if( newLevel < 0 ) {
+            throw "Invalid logging level specified: " + name;
+          } else {
+            level = newLevel;
+          }
+
+          return self;
+        }
+
+        return logLevels[level];
+      };
+
+      this.isLevelEnabled = function( name ) {
+        return logLevels.indexOf( name.toUpperCase() ) >= level;
+      };
+
+      this.loggingEnabled = function (flag) {
+          if (angular.isDefined(flag)) {
+              loggingEnabled = !!flag;
+              return self;
+          }
+
+          return loggingEnabled;
+      };
+
+
+      this.logToConsole = function (flag) {
+        if (angular.isDefined(flag)) {
+          logToConsole = !!flag;
+          return self;
+        }
+
+        return logToConsole;
+      };
+
+      this.$get = [ '$injector', function ($injector) {
+
+        var lastLog = null;
+
+
+        /**
+         * Send the specified data to loggly as a json message.
+         * @param data
+         */
+        var sendMessage = function (data) {
+          //If a token is not configured, don't do anything.
+          if (!token || !loggingEnabled) {
+            return;
+          }
+
+          //TODO we're injecting this here to resolve circular dependency issues.  Is this safe?
+          var $window = $injector.get( '$window' );
+          var $location = $injector.get( '$location' );
+		   //we're injecting $http
+          var $http = $injector.get( '$http' );
+
+          lastLog = new Date();
+
+          var sentData = angular.extend({}, extra, data);
+
+          if (includeCurrentUrl) {
+            sentData.url = $location.absUrl();
+          }
+
+          if( includeTimestamp ) {
+            sentData.timestamp = lastLog.toISOString();
+          }
+
+          if( includeUserAgent ) {
+            sentData.userAgent = $window.navigator.userAgent;
+          }
+
+          //Loggly's API doesn't send us cross-domain headers, so we can't interact directly
+           //Set header
+          var config = {
+            headers: {
+             'Content-Type': 'text/plain'
+            },
+            withCredentials: false
+          };
+
+          // Apply labels
+          for (var label in labels) {
+            if (label in sentData) {
+              sentData[labels[label]] = sentData[label];
+              delete sentData[label];
+            }
+          }
+
+          //Ajax call to send data to loggly
+          $http.post(buildUrl(),sentData,config);
+        };
+
+        var attach = function() {
+        };
+
+        var inputToken = function(s) {
+          if (angular.isDefined(s)) {
+            token = s;
+          }
+
+          return token;
+        };
+
+        return {
+          lastLog: function(){ return lastLog; },
+          sendConsoleErrors: function(){ return sendConsoleErrors; },
+          level : function() { return level; },
+          loggingEnabled: self.loggingEnabled,
+          isLevelEnabled : self.isLevelEnabled,
+          attach: attach,
+          sendMessage: sendMessage,
+          logToConsole: logToConsole,
+          inputToken: inputToken,
+
+          /**
+           * Get or set the fields to be sent with all logged events.
+           * @param d
+           * @returns {*}
+           */
+          fields: function( d ) {
+            if( angular.isDefined( d ) ) {
+              self.fields( d );
+            }
+            return self.fields();
+          }
+        };
+      }];
+
+    } );
+
+
+  angular.module( 'logglyLogger', ['logglyLogger.logger'] )
+    .config( [ '$provide', function( $provide ) {
+
+      $provide.decorator('$log', [ "$delegate", '$injector', function ( $delegate, $injector ) {
+
+        var logger = $injector.get('LogglyLogger');
+
+        // install a window error handler
+        if(logger.sendConsoleErrors() === true) {
+          var _onerror = window.onerror;
+
+          //send console error messages to Loggly
+          window.onerror = function (msg, url, line, col, error) {
+            logger.sendMessage({
+              level : 'ERROR',
+              message: msg,
+              line: line,
+              col: col,
+              stack: error && error.stack
+            });
+
+            if (_onerror && typeof _onerror === 'function') {
+              _onerror.apply(window, arguments);
+            }
+          };
+        }
+
+        var wrapLogFunction = function(logFn, level, loggerName) {
+
+          var wrappedFn = function () {
+            var args = Array.prototype.slice.call(arguments);
+
+            if(logger.logToConsole) {
+              logFn.apply(null, args);
+            }
+
+            // Skip messages that have a level that's lower than the configured level for this logger.
+            if(!logger.loggingEnabled() || !logger.isLevelEnabled( level ) ) {
+              return;
+            }
+
+            var msg = (args.length === 1 ? args[0] : args) || {};
+            var sending = { level: level };
+
+            if(angular.isDefined(msg.stack) || (angular.isDefined(msg[0]) && angular.isDefined(msg[0].stack))) {
+              //handling console errors
+              if(logger.sendConsoleErrors() === true){
+                sending.message = msg.message || msg[0].message;
+                sending.stack = msg.stack || msg[0].stack;
+              }
+              else {
+                return;
+              }
+            }
+            else if(angular.isObject(msg)) {
+              //handling JSON objects
+              sending = angular.extend({}, msg, sending);
+            }
+            else{
+              //sending plain text
+              sending.message = msg;
+            }
+
+            if( loggerName ) {
+              sending.logger = msg;
+            }
+
+            //Send the message to through the loggly sender
+            logger.sendMessage( sending );
+          };
+
+          wrappedFn.logs = [];
+
+          return wrappedFn;
+        };
+
+        var _$log = (function ($delegate) {
+          return {
+            log: $delegate.log,
+            info: $delegate.info,
+            warn: $delegate.warn,
+            error: $delegate.error
+          };
+        })($delegate);
+
+        var getLogger = function ( name ) {
+          return {
+            log:    wrapLogFunction( _$log.log, 'INFO', name ),
+            debug:  wrapLogFunction( _$log.debug, 'DEBUG', name ),
+            info:   wrapLogFunction( _$log.info, 'INFO', name ),
+            warn:   wrapLogFunction( _$log.warn, 'WARN', name ),
+            error:  wrapLogFunction( _$log.error, 'ERROR', name )
+          };
+        };
+
+        //wrap the existing API
+        $delegate.log =    wrapLogFunction($delegate.log, 'INFO');
+        $delegate.debug =  wrapLogFunction($delegate.debug, 'DEBUG');
+        $delegate.info =   wrapLogFunction($delegate.info, 'INFO');
+        $delegate.warn =   wrapLogFunction($delegate.warn, 'WARN');
+        $delegate.error =  wrapLogFunction($delegate.error, 'ERROR');
+
+        //Add some methods
+        $delegate.getLogger = getLogger;
+
+        return $delegate;
+      }]);
+
+    }]);
+
+
+
+})(window.angular);
